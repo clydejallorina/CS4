8994e5d58b91c7a789484ffdb1856d821f703337
clydejallorina <clydejallorina@gmail.com>
2016-09-02 08:31:30 +0800

    lol wtf is a comsci anyway
    comsci is not a real word

---
+++ ME3.java
@@ -0,0 +1,53 @@
+public class ME3{
+
+	public static int slope(int x1, int x2, int y2, int y2){
+		return x1;
+	}
+
+	public static bool isLeap(int year){
+		return true;
+	}
+
+	public static float finalgwa(float grade){
+		float gwa;
+		return gwa;
+	}
+	
+	public static void main(String[] args){
+		/*
+			The slope of the line that passes through points (x1,y1) and (x2,y2) is:
+							(y2 - y1)/(x2 - x1)
+			
+			Write a Java method that asks user for the values x1, y1, x2, y2 and returns 
+			the slope of the line passing through the points (x1,y1) and (x2,y2).
+			Use BufferedReafer instead of Java scanner.
+
+
+		*/
+
+		/*
+			Leap year
+			A year is a leap year if it is divisible by 4 but not by 100, except that it can be divisible by 400.
+			Write a Java methd that asks for a year from the user. It will determine if a given year is a leap year.
+			Use BufferedReafer instead of Java scanner.
+ 
+		*/
+
+		/*
+			General Weighted Average
+
+			Write a method that will display the corresponding final grade of a student given the general weighted average. Use the following table for reference:
+
+			92-100		1.0
+			88-91		1.25
+			85-87		1.50
+			82–84		1.75
+			78-81		2.00
+			74-77		2.25
+			70-73		2.50
+			65-69		2.75
+			60-64		3.00
+			below 60	5.00
+		*/
+	}
+}
